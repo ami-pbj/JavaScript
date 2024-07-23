30dc01460b34c6250a4ceef2ecba75987903b8e5
PURNENDU BIKASH JANA <95628601+ami-pbj@users.noreply.github.com>
2024-07-23 15:24:42 +0000

    variables

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,16 @@
+const customerId = 45362;
+let customerEmail = "customer@example.com";
+var customerPassword = "password";
+
+// showing output on console
+console.log(customerId);
+
+// showing multiple outputs on console
+console.log(customerId, customerEmail, customerPassword);
+
+// showing multiple outputs on console => Array Format
+console.log([customerId, customerEmail, customerPassword]);
+
+// showing outputs on console in table format
+// Console Table will accept data only in Array format
+console.table([customerId, customerEmail, customerPassword]);
