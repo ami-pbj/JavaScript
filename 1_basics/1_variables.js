6612b0b11c1bd76ffc101297a40434d070caa44c
PURNENDU BIKASH JANA <95628601+ami-pbj@users.noreply.github.com>
2024-07-23 15:44:09 +0000

    completed variables

--- 1_basics/1_variables.js
+++ 1_basics/1_variables.js
@@ -14,3 +14,28 @@ console.log([customerId, customerEmail, customerPassword]);
 // showing outputs on console in table format
 // Console Table will accept data only in Array format
 console.table([customerId, customerEmail, customerPassword]);
+
+
+
+// Let vs Var in JavaScript
+/*
+Let : always prefer Let rather than Var
+Var : Prefer not to use var because of issue in block scope and functional scope
+*/
+
+
+// Overwrite same variables
+// Let can be overwrites
+customerEmail = "overwrite@example.com"
+console.log(customerEmail);
+console.log(typeof customerEmail);
+
+// Var can be overwrites
+customerPassword = "overwritePassword"
+console.log(customerPassword);
+console.log(typeof customerPassword);
+
+// Const can't be overwritten
+// customerId = 27;
+// console.log(customerAge);
+// console.log(typeof customerAge);
